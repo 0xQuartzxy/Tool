7faeab20b9dd894379a367fc8747a84c30783ea2
JacobTheWorst <133568650+0xQuartzxy@users.noreply.github.com>
2023-05-14 16:21:05 +0700

    Create load.lua

---
+++ load.lua
@@ -0,0 +1,10 @@
+repeat wait() until game.Players
+repeat wait() until game.Players.LocalPlayer
+repeat wait() until game.ReplicatedStorage
+repeat wait() until game.ReplicatedStorage:FindFirstChild("Remotes");
+repeat wait() until game.Players.LocalPlayer:FindFirstChild("PlayerGui");
+repeat wait() until game.Players.LocalPlayer.PlayerGui:FindFirstChild("Main");
+repeat wait() until game:GetService("Players")
+repeat wait() until game:GetService("Players").LocalPlayer.Character:FindFirstChild("Energy")
+
+if not game:IsLoaded() then repeat game.Loaded:Wait() until game:IsLoaded() end
